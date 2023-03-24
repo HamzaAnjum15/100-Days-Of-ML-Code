f2e5bad8a9fa4c3a1af42ed542f9d23df5671eab
Hamza Anjum <66873696+HamzaAnjum15@users.noreply.github.com>
2023-03-24 23:52:47 +0500

    Add files via upload

---
+++ Data Types/DataTypes.py
@@ -0,0 +1,32 @@
+# A data type refers to the kind of data that a variable holds
+
+#Numbers
+#These include integers, floating-point numbers, and complex numbers
+a = 5 #Integer
+b = 7.6 #Floating point number
+
+#String
+# These are sequences of characters, enclosed in single or double quotes
+c = "Hello World"
+
+#Boolean
+#This is a type that can only have one of two values, True or False
+d = True
+e = False
+
+#List
+#These are ordered collections of items, which can be of any type
+f = [1, 3, "hello", 5.4]
+
+#Tuple
+#These are similar to lists, but are immutable (cannot be changed after creation)
+g = (1, 2, 3)
+
+#Sets
+# These are unordered collections of unique items
+h = {4, 5, 3}
+
+#Dictionaries
+#These are unordered collections of key-value pairs, where each key must be unique
+i = {"name" : "hamza", "age" : "21"}
+
