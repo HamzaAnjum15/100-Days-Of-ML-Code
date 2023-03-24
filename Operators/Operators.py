5ef3115854fa9c27a4987d3c5ea8daefcc04ae0b
Hamza Anjum <66873696+HamzaAnjum15@users.noreply.github.com>
2023-03-24 16:04:22 +0500

    Add files via upload

---
+++ Operators/Operators.py
@@ -0,0 +1,26 @@
+# An operator is a symbol or a character that represents a specific operation
+
+#Arithmetic operators
+#These operators are used to perform basic arithmetic operations such as addition, subtraction, multiplication, division, and modulus
+a = 5
+b = 6
+a + b #Addition
+a - b #Subtraction
+a * b #Multiplication
+a / b #Division
+a % b #Modulus
+
+# Comparison Operators
+# These operators are used to compare two values or variables
+a == b #Equal to
+a != b #Not equal to
+a > b #Greater than
+a < b #Less than
+a >= b #Greater or equal to
+a <= b #Less or equal to
+
+#Logical Operators
+#These operators are used to combine conditional statements.
+a and b #And 
+a or b #Or
+not b #Not
